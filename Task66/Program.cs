ee9c47fad43e535e163a475766a1493531d5ad0a
Simon Nevzorov <sem.nevzorov@gmail.com>
2023-03-26 23:53:36 +0300

    домашнее задание к семинару 9

---
+++ Task66/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a<b)
+{
+    int temp1 = a;
+    a = b;
+    a = temp1;
+}
+
+ int CountNumbers(int a, int b )
+{   
+    int result =a;
+    if (a==b)
+        return 0;
+        else 
+        {
+         a++;
+         result = a+ CountNumbers(a,b);
+        return result;
+        }
+}
+
+
+Console.WriteLine(CountNumbers(a-1,b));
---
+++ Task68/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Function (int m, int n)
+{
+    if (m == 0)
+        {
+        return n + 1;
+        }
+    else if (n == 0 && m > 0)
+        {
+        return Function (m - 1, 1);
+        }
+    else
+        {
+        return Function (m - 1, Function (m, n - 1));
+        }
+}
+
+ Console.WriteLine(Function(m,n));
+
+
+// bool IfEven (int value)
+// {
+//     return value % 2 == 0;
+// }
+// Console.WriteLine(IfEven(10));
